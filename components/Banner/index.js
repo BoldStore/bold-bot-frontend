d3e8f251585ee1f9fedffd3d2849bd638e3eb1ca
aryanteng <aryanteng892@gmail.com>
2022-07-24 23:13:15 +0530

    facebook login button completed, globals updated

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import styles from "./styles.module.css";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
 
 function Banner() {
-  return <div>Banner</div>;
+  return (
+    <div>
+      <button className={styles.button}>
+        <FacebookRoundedIcon
+          sx={{ color: "var(--white)" }}
+          className={styles.icon}
+        />
+        <p>Continue With Facebook</p>
+      </button>
+    </div>
+  );
 }
 
 export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Feature from "../components/Feature";
 import Client from "../components/ClientsWithUs/Client";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <meta name="description" content="" />
         <link rel="icon" href="/assets/Logo.png" />
       </Head>
+      <Banner />
       <Client />
       <Feature />
       <Feature isReverse={true} />
