8f82680976d21eb59a16db957f1b66997072cc72
aryanteng <aryanteng892@gmail.com>
2022-07-24 16:15:25 +0530

    structure defined and files initialised

---
+++ components/ClientsWithUs/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './client.module.css'
+
+function ClientsWithUs() {
+  return (
+    <div>ClientsWithUs</div>
+  )
+}
+
+export default ClientsWithUs
---
+++ components/Feature/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+function Feature() {
+  return (
+    <div>Feature</div>
+  )
+}
+
+export default Feature
