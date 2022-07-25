ffadb8ec3734f81959ab2f0aa1226a9b1e861e9f
avivashishta29 <avivashishta@gmail.com>
2022-07-25 18:54:54 -0400

    header done

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./styles.module.css";
 import Link from "next/link";
 import DrawerComponent from "./DrawerComponent";
