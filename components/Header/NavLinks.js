82a1bcc908b688dd7d70e291d977b76a73d10af6
aryanteng <aryanteng892@gmail.com>
2022-07-26 15:59:31 +0530

    mobile header fixed

--- components/Header/NavLinks.js
+++ components/Header/NavLinks.js
@@ -1,6 +1,3 @@
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import { Link } from "@material-ui/core";
 import React from "react";
 import styles from "./styles.module.css";
