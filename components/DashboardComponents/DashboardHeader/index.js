912522905d4863885451ed3ceef203804a34ce78
avivashishta29 <avivashishta@gmail.com>
2022-07-28 10:46:26 -0400

    dashboard scrolling fix

--- components/DashboardComponents/DashboardHeader/index.js
+++ components/DashboardComponents/DashboardHeader/index.js
@@ -7,9 +7,10 @@ function DashboardHeader() {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.title}>
+        {/* <div className={styles.title}>
           <h6>BOLDbot</h6>
-        </div>
+        </div> */}
+        <br />
         <Link href="/dashboard/">
           <p className={styles.link}>Home</p>
         </Link>
