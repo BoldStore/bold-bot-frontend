4c29a7bdfa9cbef5f5672c9e7372ced33681f557
aryanteng <aryanteng892@gmail.com>
2022-08-02 13:36:58 +0530

    fixed responsive errors

--- components/DashboardComponents/DashboardHeader/topHeader.js
+++ components/DashboardComponents/DashboardHeader/topHeader.js
@@ -4,6 +4,7 @@ function TopHeader() {
   return (
     <div className={styles.navbar}>
       <h4 className={styles.logo}>BOLDBOT</h4>
+
       <div className={styles.linksContainer}>
         <img src="https://i.ibb.co/myvq6GR/aryan.jpg" className={styles.user} />
         <p>Aryan Teng</p>
