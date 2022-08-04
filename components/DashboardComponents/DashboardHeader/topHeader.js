99b2503a5856ac2bc2349683fdaad5672cc1234c
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-08-04 17:31:07 +0530

    Header

--- components/DashboardComponents/DashboardHeader/topHeader.js
+++ components/DashboardComponents/DashboardHeader/topHeader.js
@@ -1,13 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
 import React from "react";
+import { useSelector } from "react-redux";
 import styles from "./styles.module.css";
+
 function TopHeader() {
+  const userState = useSelector((state) => state.user);
+
   return (
     <div className={styles.navbar}>
-      <h4 className={styles.logo}>BOLDBOT</h4>
+      <Link href={"/dashboard"} passHref={true}>
+        <h4 className={styles.logo}>BOLDBOT</h4>
+      </Link>
 
       <div className={styles.linksContainer}>
-        <img src="https://i.ibb.co/myvq6GR/aryan.jpg" className={styles.user} />
-        <p>Aryan Teng</p>
+        <img
+          src={
+            userState?.user?.profile_pic ?? "https://i.ibb.co/myvq6GR/aryan.jpg"
+          }
+          className={styles.user}
+          alt="User Pic"
+        />
+        <p>{userState?.user?.name ?? "Name"}</p>
       </div>
     </div>
   );
