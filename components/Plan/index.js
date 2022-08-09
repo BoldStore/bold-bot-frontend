bb7d7c361cba5fb91cae89cd0a45068d60ca88ac
aryanteng <aryanteng892@gmail.com>
2022-08-09 16:09:27 +0530

    our plans

--- components/Plan/index.js
+++ components/Plan/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import styles from "./styles.module.css";
 import DashboardButton from "../DashboardComponents/DashboardButton";
+import EmojiPeopleRoundedIcon from "@mui/icons-material/EmojiPeopleRounded";
+import AcUnitRoundedIcon from "@mui/icons-material/AcUnitRounded";
 
 function Plan(planName, planDesc, planPrice) {
   return (
     <div className={styles.container}>
-      <h5>Starter Pack</h5>
+      <div className={styles.packName}>
+        <h5>Starter Pack</h5>
+        <div>
+          <EmojiPeopleRoundedIcon className={styles.icon} />
+          <AcUnitRoundedIcon className={styles.icon} />
+        </div>
+      </div>
       <h6>
         The persistent menu removes the miscommunication quotient by providing
         the option of starting over or redirecting to a new topic.
