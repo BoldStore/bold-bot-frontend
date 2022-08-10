193386e7246814318be4e5e75d87d0b24cf7baff
aryanteng <aryanteng892@gmail.com>
2022-08-10 17:06:26 +0530

    custom loader completed

--- components/Loader/index.js
+++ components/Loader/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import styles from "./styles.module.css";
-import { ScaleLoader } from "react-spinners";
+import { motion } from "framer-motion";
 
 function Loader() {
   return (
     <div className={styles.container}>
-      <ScaleLoader color="var(--blue)" height={50} />
+      <h1>
+        BOLD<span style={{ color: "var(--blue)" }}>bot</span>
+      </h1>
+      <div className={styles.loaderParent}>
+        <motion.div
+          className={styles.loaderChild}
+          animate={{
+            x: [-50, 200, -50],
+          }}
+          transition={{
+            duration: 1.25,
+            ease: "easeInOut",
+            repeat: Infinity,
+            type: "spring",
+          }}
+        />
+      </div>
     </div>
   );
 }
