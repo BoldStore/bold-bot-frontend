232ce7d2bd7d4d1e12d8c72821e1d3bd1f9789b3
avivashishta29 <avivashishta@gmail.com>
2022-08-10 16:16:46 -0400

    plans

--- components/OurPlans/plans.js
+++ components/OurPlans/plans.js
@@ -2,21 +2,21 @@ export const plans = [
   {
     planName: "Starter Plan",
     planDesc:
-      "The persistent menu removes the miscommunication quotient by providing the option of starting over or redirecting to a new topic.",
+      "This starter plan elevates your dm’s to the next level by including the simplest assets to automating your interactions— custom greetings and ice breakers. ",
     planPrice: "₹500",
     icons: ["greeting", "ice_breakers"],
   },
   {
     planName: "Standard Plan",
     planDesc:
-      "The persistent menu removes the miscommunication quotient by providing the option of starting over or redirecting to a new topic.",
+      "The standard plan includes all the superior features of the starter pack with the addition of the persistent menu. ",
     planPrice: "₹1000",
     icons: ["ice_breakers", "persistent_menu", "website_link"],
   },
   {
     planName: "Super Plan",
     planDesc:
-      "The persistent menu removes the miscommunication quotient by providing the option of starting over or redirecting to a new topic.",
+      "Includes all features from the starter and standard packages, the super plan goes one step above with the addition of the custom replies.",
     planPrice: "₹1500",
     icons: ["greeting", "ice_breakers", "persistent_menu", "custom_replies"],
   },
