f407ffad51b78c792eec3bb2cc28e88bde532f22
aryanteng <aryanteng892@gmail.com>
2022-08-16 17:12:31 +0530

    google analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import "../styles/globals.css";
-import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { wrapper } from "../store/configureStore";
