66f308f25728498e5c57d073f5b51bd5ea163e00
aryanteng <aryanteng892@gmail.com>
2022-08-16 17:07:53 +0530

    google analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ import { getUser } from "../store/actions/user";
 import "../styles/App.scss";
 import Script from "next/script";
 import SEO from "../components/SEO";
+import { GOOGLE_ANALYTICS_ID } from "../constants";
 
 function MyApp({ Component, pageProps }) {
   const dispatch = useDispatch();
@@ -26,22 +27,22 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Component {...pageProps} />
-      <ToastContainer autoClose={false} />
-      {/* <!-- Google tag (gtag.js) --> */}
+      <SEO />
       <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-1ZBB6TQ926"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
       />
       <Script strategy="lazyOnload" id="google-analytics-script">
         {`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-1ZBB6TQ926'), {
+        gtag('config', ${GOOGLE_ANALYTICS_ID}), {
           page_path: window.location.pathname,
         });`}
       </Script>
-      <SEO />
+      <Component {...pageProps} />
+      <ToastContainer autoClose={false} />
+      {/* <!-- Google tag (gtag.js) --> */}
     </>
   );
 }
