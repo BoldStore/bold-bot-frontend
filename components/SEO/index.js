10c9c8a8d454de2c355e3574da3eea1d1d7c6f78
aryanteng <aryanteng892@gmail.com>
2022-08-18 00:55:38 +0530

    seo

--- components/SEO/index.js
+++ components/SEO/index.js
@@ -29,7 +29,7 @@ function SEO({ title, desc }) {
         {
           property: "og:image",
           itemprop: "image",
-          content: "https://i.ibb.co/XbWkxPY/banner.png",
+          content: "/assets/Logo.svg",
         },
         {
           name: "msapplication-TileImage",
