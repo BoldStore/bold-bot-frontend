266cf4bc9e67b22fcf843cbbd465dd19346b8a2c
aryanteng <aryanteng892@gmail.com>
2022-08-17 21:50:54 +0530

    next-seo

--- components/SEO/index.js
+++ components/SEO/index.js
@@ -1,23 +1,37 @@
 import React from "react";
-import Head from "next/head";
+import { WEB_URL } from "../../constants";
+import { NextSeo } from "next-seo";
 
-function SEO({ title, desc, imgSrc }) {
+function SEO({ title, desc }) {
   return (
-    <Head>
-      <title>{title ?? "BOLDbot"}</title>
-      <meta
-        name="description"
-        content={
-          desc ??
-          "BOLDbot is the next step in automating customer service for Instagram businesses. It’s personalised greetings and range of choices increases the ease with which your patrons can inquire or communicate with the store via direct messages. The persistent menu removes the miscommunication quotient by providing the option of starting over or redirecting to a new topic. BOLDbot levels up the professionalism of your business by increasing efficacy and simplifying interaction with your consumer. Automate your business, the bold way."
-        }
-      />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1"
-      ></meta>
-      <link rel="icon" href={imgSrc ?? "/assets/Logo.svg"} />
-    </Head>
+    <NextSeo
+      title={title}
+      defaultTitle="BOLDbot"
+      description={
+        desc ??
+        "BOLDbot is the next step in automating customer service for Instagram businesses. It levels up the professionalism of your business by increasing efficacy and simplifying interaction with your consumer."
+      }
+      keywords
+      canonical={`${WEB_URL}`}
+      twitter={{
+        title: "BOLDbot",
+        site: WEB_URL,
+        cardType: "/assets/Logo.svg",
+        description:
+          "BOLDbot is the next step in automating customer service for Instagram businesses. It levels up the professionalism of your business by increasing efficacy and simplifying interaction with your consumer.",
+      }}
+      additionalLinkTags={[
+        {
+          rel: "icon",
+          href: "/assets/Logo.svg",
+        },
+        {
+          rel: "apple-touch-icon",
+          href: "/assets/Logo.svg",
+          sizes: "76x76",
+        },
+      ]}
+    />
   );
 }
 
