9e76ad42d2a2d3b58bc2c3cb955d908ffba2095e
avivashishta29 <avivashishta@gmail.com>
2022-08-20 01:43:24 +0530

    access image change

--- components/DashboardComponents/Lists/prerequisites.js
+++ components/DashboardComponents/Lists/prerequisites.js
@@ -25,7 +25,7 @@ export const prerequisites = [
       "/assets/Prerequisites/tapSettings2.png",
       "/assets/Prerequisites/tapPrivacy.png",
       "/assets/Prerequisites/tapMessages.png",
-      "/assets/Prerequisites/allowAccess.png",
+      "/assets/Prerequisites/step4.png",
     ],
     title: "Allow Access to Messages on Instagram.",
     desc: "Ice Breakers provide a way for users to start a conversation with a business with a list of frequently asked questions. A maximum of 4 questions can be set via the Ice Breaker API. Starting Oct 19th, 2021, Ice Breakers supports localization to allow businesses to set custom questions depending on the user locale. The API will have a new format and we encourage developers to leverage the new format to set and retrieve Ice Breakers information. The list of supported locales can be found here.",
