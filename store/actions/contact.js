d7164a5db7c1ed7bbc2ea3b66b00ae9286309ed6
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-08-21 23:41:00 +0530

    Contact things

--- store/actions/contact.js
+++ store/actions/contact.js
@@ -2,11 +2,14 @@ import instance from "../../axios";
 import { CONTACT_US } from "../../constants";
 import * as ActionTypes from "../ActionTypes";
 
-export const getUser = () => {
+export const contactBold = (name, insta_username) => {
   return async (dispatch) => {
     dispatch({ type: ActionTypes.CONTACT_US_REQUEST });
     try {
-      const response = await instance.get(CONTACT_US);
+      const response = await instance.post(CONTACT_US, {
+        name,
+        insta_username,
+      });
       dispatch({
         type: ActionTypes.CONTACT_US_SUCCESS,
         data: response.data,
