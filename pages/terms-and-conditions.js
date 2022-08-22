c3ac14b1c2ae566fbf37185d63682403f9b35b01
aryanteng <aryanteng892@gmail.com>
2022-08-22 15:07:27 +0530

    t&c

--- pages/terms-and-conditions.js
+++ pages/terms-and-conditions.js
@@ -3,33 +3,42 @@ import React from "react";
 import Header from "../components/Header/Header";
 import styles from "../styles/Policy.module.css";
 import Link from "next/link";
+
 function TermsAndConditions() {
   return (
-    <div>
+    <>
       <Header />
+      <img
+        src={"/assets/automateBanner.png"}
+        className={styles.banner}
+        width="100%"
+        alt="Banner"
+      />
       <div className={styles.container}>
-        <img src={"/assets/Banner.png"} width="100%" alt="Banner" />
-        <h2>Terms and Conditions[Last Edited on 22nd August 2022].</h2>
+        <h2>
+          Terms and Conditions
+          <br />
+          [Last Edited on 22nd August 2022].
+        </h2>
         <br />
         <p>
-          BOLDbot (&apos;[https://boldbot.in/]&apos;) owns and operates this
-          Website. This document governs your relationship with
-          [https://boldbot.in/] (&apos;Website&apos;). Access to and use of this
-          Website and the products and services available through this Website
-          (collectively, the &apos;Services&apos;) are subject to the following
-          terms, conditions and notices (the &apos;Terms of Service&apos;). By
-          using the Services, you are agreeing to all of the Terms of Service,
-          as may be updated by us from time to time. You should check this page
-          regularly to take notice of any changes we may have made to the Terms
-          of Service.
+          BOLDbot (“[https://boldbot.in/]”) owns and operates this Website. This
+          document governs your relationship with [https://boldbot.in/]
+          (“Website”). Access to and use of this Website and the products and
+          services available through this Website (collectively, the
+          &quot;Services&quot;) are subject to the following terms, conditions
+          and notices (the &quot;Terms of Service&quot;). By using the Services,
+          you agree to all of the Terms of Service, as may be updated by us from
+          time to time. You should check this page regularly to take notice of
+          any changes we may have made to the Terms of Service.
         </p>
         <p>
           Access to this Website is permitted on a temporary basis, and we
           reserve the right to withdraw or amend the Services without notice. We
-          will not be liable if for any reason this Website is unavailable at
+          will not be liable if, for any reason, this Website is unavailable at
           any time or for any period. From time to time, we may restrict access
           to some parts or all of this Website. This Website may contain links
-          to other websites (the &apos;Linked Sites&apos;), which are not
+          to other websites (the &quot;Linked Sites&quot;), which are not
           operated by [https://boldbot.in/]. [https://boldbot.in/] has no
           control over the Linked Sites and accepts no responsibility for them
           or for any loss or damage that may arise from your use of them. Your
@@ -38,72 +47,74 @@ function TermsAndConditions() {
         </p>
         <h3>Privacy Policy</h3>
         <p>
-          Our Privacy Policy forms part of and must be read in conjunction with,
+          Our Privacy Policy forms part of and must be read in conjunction with
           Mobile Application Terms and Conditions. We reserve the right to
           change this Privacy Policy at any time.
           <br />
-          Visit -
+          Visit - {""}
           <Link href="/privacy-policy" passHref={true}>
-            www.boldbot.in/privacy-policy
+            <span style={{ color: "var(--blue)", textDecoration: "underline" }}>
+              www.boldbot.in/privacy-policy
+            </span>
           </Link>
         </p>
         <h3>General</h3>
         <p>
           <strong>Customers</strong> permitting access to information such as
-          Facebook pages further grant BOLDbot to use their information as
-          provided by Instagram and Facebook’s Messenger API, Graph API, etc.
-          The customer’s personal information such as :- <br />
+          Facebook pages further grant BOLDbot to use their data as provided by
+          Instagram and Facebook’s Messenger API, Graph API, etc. The customer’s
+          personal information, such as :- <br />
           1. Name <br />
           2. FacebookPage and ID <br />
           3. Instagram Page and ID <br />
           4. Instagram username and dp <br />
           5. Instagram DM related data <br />
           6. Payment plans <br />
-          Is the data used by BOLDbot solely to make customer experience more
+          is the data used by BOLDbot solely to make customer experience more
           efficient following the sign-up process. <br />
           <br />
         </p>
         <p>
-          All data ( FB page, DMs, Conversations, etc) acquired from social
+          All data (FB page, DMs, Conversations, etc.) acquired from social
           media handles such as Instagram is used by the app to better
           consolidate the profile on boldbot.in. Concerning plans, the website
-          does track data pertaining to payments. In this regard our claim to
+          does track data pertaining to payments. In this regard, our claim to
           privacy remains the same for every customer.
         </p>
         <p>
           We respect the privacy of our users and every person who visits our
-          Mobile Application BOLDbot. Here at BOLDbot, we are committed to
+          Mobile Application, BOLDbot. Here at BOLDbot, we are committed to
           protecting your personal information and your right to privacy. If you
-          have any questions or concerns about our policy or our practices with
-          regards to your personal information, please contact us at
+          have any questions or concerns about our policy or our practices
+          regarding your personal information, please contact us at
           boldbotofficial@gmail.com.
         </p>
         <p>
-          When you visit our Mobile Application BOLDbot (&apos;Mobile
-          Application&apos;) and use our services, you trust us with your
-          personal information. We take your privacy very seriously. In this
-          privacy notice, we describe our privacy policy. We seek to explain to
-          you in the clearest way possible what information we collect, how we
-          use it, and what rights you have concerning it. We hope you take some
-          time to read through it carefully, as it is important. If there are
-          any terms in this privacy policy that you do not agree with, please
-          discontinue the use of our Mobile Application and our services.
+          When you visit our Mobile Application BOLDbot (“Mobile Application”)
+          and use our services, you trust us with your personal information. We
+          take your privacy very seriously. In this privacy notice, we describe
+          our privacy policy. We seek to explain to you in the most precise way
+          possible what information we collect, how we use it, and what rights
+          you have concerning it. We hope you take some time to read through it
+          carefully, as it is essential. If there are any terms in this privacy
+          policy that you do not agree with, please discontinue the use of our
+          Mobile Application and our services.
         </p>
         <h3>Prohibitions</h3>
         <p>
           You must not misuse this Website. You will not: commit or encourage a
-          criminal offense; transmit or distribute a virus, trojan, worm, logic
+          criminal offence; transmit or distribute a virus, trojan, worm, logic
           bomb or any other material which is malicious, technologically
           harmful, in breach of confidence or in any way offensive or obscene;
           hack into any aspect of the Service; corrupt data; cause annoyance to
           other users; infringe upon the rights of any other person&apos;s
           proprietary rights; send any unsolicited advertising or promotional
-          material, commonly referred to as "spam"; or attempt to affect the
-          performance or functionality of any computer facilities of or accessed
-          through this Website. Breaching this provision would constitute a
-          criminal offense and [https://boldbot.in/] will report any such breach
-          to the relevant law enforcement authorities and disclose your identity
-          to them.
+          material, commonly referred to as &quot;spam&quot; or attempt to
+          affect the performance or functionality of any computer facilities of
+          or accessed through this Website. Breaching this provision would
+          constitute a criminal offence, and [https://boldbot.in/] will report
+          any such breach to the relevant law enforcement authorities and
+          disclose your identity to them.
         </p>
         <p>
           We will not be liable for any loss or damage caused by a distributed
@@ -117,7 +128,7 @@ function TermsAndConditions() {
         <p>
           The intellectual property rights in all software and content
           (including photographic images) made available to you on or through
-          this Website remains the property of [https://boldbot.in/] or its
+          this Website remain the property of [https://boldbot.in/] or its
           licensors and are protected by copyright laws and treaties around the
           world. All such rights are reserved by [https://boldbot.in/] and its
           licensors. You may store, print and display the content supplied
@@ -129,7 +140,7 @@ function TermsAndConditions() {
         </p>
         <h3>Terms of Sale</h3>
         <p>
-          By buying a plan you are offering to use our services for a minimum
+          By buying a plan, you are offering to use our services for a minimum
           period of a month which can be extended indefinitely and is subject to
           the following terms and conditions. All plans are subject to
           availability and confirmation as well.
@@ -138,22 +149,22 @@ function TermsAndConditions() {
           In order to contract with [https://boldbot.in/] you must be over 18
           years of age and possess a valid credit or debit card issued by a bank
           acceptable to us. [https://boldbot.in/] retains the right to refuse
-          any request made by you. If your purchase is accepted we will inform
-          you by email and we will confirm the identity of the party which you
+          any request made by you. If your purchase is accepted, we will inform
+          you by email, and we will confirm the identity of the party which you
           have contracted with. This will usually be [https://boldbot.in/] or
-          may in some cases be a third party. Where a contract is made with a
+          may in some cases, be a third party. Where a contract is made with a
           third party [https://boldbot.in/] is not acting as either agent or
-          principal and the contract is made between yourself and that third
+          principal, and the contract is made between yourself and that third
           party and will be subject to the terms of sale which they supply you.
-          When buying a plan you undertake that all details you provide to us
-          are true and accurate, that you are an authorized user of the credit
+          When buying a plan, you undertake that all details you provide to us
+          are true and accurate, that you are an authorised user of the credit
           or debit card used to place your order and that there are sufficient
           funds to cover the cost of the goods. The cost of foreign plans and
           services may fluctuate. All prices advertised are subject to such
           changes.
         </p>
         <p>
-          (a) Our Contract When you purchase a plan, you will receive an
+          (a) Our Contract: When you purchase a plan, you will receive an
           acknowledgement e-mail confirming receipt of your order: this email
           will only be an acknowledgement and will not constitute acceptance of
           your order. A contract between us will not be formed until we send you
@@ -163,12 +174,12 @@ function TermsAndConditions() {
           formed.
         </p>
         <p>
-          (b) Pricing and Availability Whilst we try and ensure that all
+          (b) Pricing and Availability: Whilst we try and ensure that all
           details, descriptions and prices which appear on this Website are
           accurate, errors may occur. If we discover an error in the price of
-          any plans which you have ordered we will inform you of this as soon as
-          possible and give you the option of reconfirming your order at the
-          correct price or cancelling it. If we are unable to contact you we
+          any plans which you have ordered, we will inform you of this as soon
+          as possible and give you the option of reconfirming your order at the
+          correct price or cancelling it. If we are unable to contact you, we
           will not treat the order as cancelled. If you cancel and you have
           already paid for the service, you may or may not receive a full
           refund. <br />
@@ -177,20 +188,23 @@ function TermsAndConditions() {
           2. Standard - ₹1000/- per month <br />
           3. Super - ₹1500/- per month <br />
           The services these plans offer are listed on the website -
-          https://boldbot.in/#OurPlans
+          <Link href={"/#OurPlans"}>
+            <span style={{ color: "var(--blue)", textDecoration: "underline" }}>
+              https://boldbot.in/#OurPlans
+            </span>
+          </Link>
         </p>
         <p>
-          (c) Payment Upon receiving your order we carry out a standard
-          authorization check on your payment card to ensure there are
-          sufficient funds to fulfill the transaction. Your card will be debited
+          (c) Payment: Upon receiving your order, we carry out a standard
+          authorisation check on your payment card to ensure there are
+          sufficient funds to fulfil the transaction. Your card will be debited
           upon authorisation being received. The monies received upon the
           debiting of your card shall be treated as a deposit against the value
-          of the services you wish to purchase. Recurring Payments are handled
-          through RazorPay. As of now, no refunds are to be accepted and neither
-          does BOLDbot accept Cash as a form of payment.
+          of the services you wish to purchase. Recurring payments are handled
+          through Razorpay. As of now, no refunds are to be accepted, and
+          neither does BOLDbot accept Cash as a form of payment.
         </p>
-
-        <h3>Linking to this website</h3>
+        <h3>Linking to this Website</h3>
         <p>
           You may link to our home page, provided you do so in a way that is
           fair and legal and does not damage our reputation or take advantage of
@@ -204,8 +218,8 @@ function TermsAndConditions() {
         </p>
         <p>
           Disclaimer as to ownership of trade marks, images of personalities and
-          third party copyright Except where expressly stated to the contrary
-          all persons (including their names and images), third party trade
+          third-party copyright Except where expressly stated to the contrary
+          all persons (including their names and images), third-party trade
           marks and content, services and/or locations featured on this Website
           are in no way associated, linked or affiliated with
           [https://boldbot.in/] and you should not rely on the existence of such
@@ -220,7 +234,7 @@ function TermsAndConditions() {
         <p>
           You agree to indemnify, defend and hold harmless
           [https://boldbot.in/], its directors, officers, employees,
-          consultants, agents, and affiliates, from any and all third party
+          consultants, agents, and affiliates, from any and all third-party
           claims, liability, damages and/or costs (including, but not limited
           to, legal fees) arising from your use this Website or your breach of
           the Terms of Service.
@@ -235,8 +249,8 @@ function TermsAndConditions() {
         <p>
           If any part of the Terms of Service is unenforceable (including any
           provision in which we exclude our liability to you) the enforceability
-          of any other part of the Terms of Service will not be affected all
-          other clauses remaining in full force and effect. So far as possible
+          of any other part of the Terms of Service will not be affected by all
+          other clauses remaining in full force and effect. As far as possible,
           where any clause/sub-clause or part of a clause/sub-clause can be
           severed to render the remaining part valid, the clause shall be
           interpreted accordingly. Alternatively, you agree that the clause
@@ -247,7 +261,7 @@ function TermsAndConditions() {
         <h3>Complaints</h3>
         <p>
           We operate a complaints handling procedure which we will use to try to
-          resolve disputes when they first arise, please let us know if you have
+          resolve disputes when they first arise; please let us know if you have
           any complaints or comments.
         </p>
         <h3>Waiver</h3>
@@ -265,7 +279,7 @@ function TermsAndConditions() {
           and signed by a Director of [https://boldbot.in/].
         </p>
       </div>
-    </div>
+    </>
   );
 }
 
