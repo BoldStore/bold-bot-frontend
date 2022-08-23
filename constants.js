c979a3023813545181055311e0451370834c749a
avivashishta29 <avivashishta@gmail.com>
2022-08-24 01:08:53 +0530

    permissions constant

--- constants.js
+++ constants.js
@@ -4,7 +4,7 @@ export const FACEBOOK_AUTH_URL = "https://www.facebook.com/v14.0/dialog/oauth";
 
 export const CLIENT_ID = "1187703702070626";
 
-export const SCOPE = "pages_read_engagement,instagram_manage_messages";
+export const SCOPE = "instagram_manage_messages";
 
 export const REDIRECT_URI = "https://boldbot.in/facebook-page/";
 
