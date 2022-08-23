f7591782fe79305ab8c310cd3bdd8cf6438d440a
avivashishta29 <avivashishta@gmail.com>
2022-08-24 01:07:02 +0530

    story replies and mentions

---
+++ components/Lists/story.js
@@ -0,0 +1,15 @@
+export const story = [
+  {
+    title: "Respone on Story Reply",
+    desc: "This is what will be sent to the user when they reply to your stories.",
+    placeholder:
+      "Hi Thankyou for replying to my story! I'll get back to you soon",
+    key: "story-reply",
+  },
+  {
+    title: "Story Mention Response",
+    desc: "This is what will be sent to the user when they mention you in their stories.",
+    placeholder: "Thankyou for mentioning me!",
+    key: "story-mention",
+  },
+];
---
+++ pages/dashboard/story.js
@@ -0,0 +1,119 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import DashboardSidebar from "../../components/DashboardComponents/DashboardSidebar";
+import InputComponent from "../../components/DashboardComponents/InputComponent";
+import { story } from "../../components/Lists/story";
+import styles from "../../styles/common.module.css";
+import DashboardButton from "../../components/DashboardComponents/DashboardButton";
+import { useDispatch, useSelector } from "react-redux";
+import { addGreeting, getGreeting } from "../../store/actions/greeting";
+import SEO from "../../components/SEO";
+
+function StoryRepliesPage() {
+  const hasPlan = true;
+  const disptach = useDispatch();
+  const user = useSelector((state) => state.user);
+  const greeting = useSelector((state) => state.greeting);
+  const [form, setForm] = useState([
+    {
+      key: "story-reply",
+      value: "",
+    },
+    {
+      key: "story-mention",
+      value: "",
+    },
+  ]);
+
+  const setValue = (e, itemKey) => {
+    const inputIndex = form.findIndex((i) => {
+      return i.key === itemKey;
+    });
+
+    const input = {
+      ...form[inputIndex],
+    };
+
+    input.value = e.target.value;
+
+    const inputs = [...form];
+    inputs[inputIndex] = input;
+
+    setForm(inputs);
+  };
+
+  useEffect(() => {
+    if (user && user.user && user?.user?.pages?.length > 0) {
+      disptach(getGreeting(user?.user?.pages[0].id));
+    }
+  }, [user.user]);
+
+  const saveGreeting = () => {
+    if (user && user.user && user?.user?.pages?.length > 0) {
+      disptach(addGreeting(user?.user?.pages[0].id, form));
+    }
+  };
+
+  useEffect(() => {
+    if (greeting?.message && greeting?.message?.texts?.length > 0) {
+      const arr = form;
+      for (let i = 0; i < greeting?.message?.texts.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+          if (arr[j].key === greeting?.message?.texts[i].key) {
+            arr[j].value = greeting?.message?.texts[i].value;
+          }
+        }
+      }
+      setForm(arr);
+    }
+  }, [greeting.message]);
+
+  useEffect(() => {
+    console.log(form);
+  }, [form]);
+
+  if (greeting.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={styles.pageDiv}>
+      <SEO
+        title={"Story Replies and Mentions"}
+        desc={
+          "Automated never means generic here at Bold. These are the first responses to a buyer’s dm. Each store can customize its greetings to cater to the needs and wants of its clientele as well as target markets. Design your own custom greetings which define you as a store when interacting with patrons."
+        }
+      />
+      <DashboardSidebar />
+      <div className={styles.container}>
+        <h3 className={styles.title}>Story Replies and Mentions</h3>
+        <p className={styles.introPara}>
+          Now you can reply to the story mentions and replies sent by customers!
+        </p>
+        <div>
+          {/* <img
+            src="/assets/greetingExample.png"
+            alt="example"
+            className={styles.img}
+          /> */}
+        </div>
+        {story.map((item, i) => (
+          <InputComponent
+            key={i}
+            title={item.title}
+            desc={item.desc}
+            value={form.find((e) => e.key == item.key).value}
+            placeholder={item.placeholder}
+            itemKey={item.key}
+            setValue={setValue}
+            disable={!hasPlan}
+          />
+        ))}
+        <DashboardButton text={"Save"} onClick={saveGreeting} />
+      </div>
+    </div>
+  );
+}
+
+export default StoryRepliesPage;
