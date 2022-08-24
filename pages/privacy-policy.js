0777359ef015a59549f1a52344fccbc965991b88
aryanteng <aryanteng892@gmail.com>
2022-08-24 23:57:25 +0530

    privacy policy

---
+++ pages/privacy-policy.js
@@ -0,0 +1,310 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Header from "../components/Header/Header";
+import SEO from "../components/SEO";
+import styles from "../styles/Policy.module.css";
+
+function PrivacyPolicy() {
+  return (
+    <>
+      <SEO title={"Privacy Policy"} />
+      <Header />
+      <img
+        src={"/assets/automateBanner.png"}
+        className={styles.banner}
+        width="100%"
+        alt="Banner"
+      />
+      <div className={styles.container}>
+        <h2>
+          Privacy Policy
+          <br />
+          [Last Edited on 24th August 2022]
+        </h2>
+        <p>
+          Our Privacy Policy forms part of and must be read in conjunction with,
+          Mobile Application Terms and Conditions. We reserve the right to
+          change this Privacy Policy at any time. We respect the privacy of our
+          users and every person who visits our Mobile Application BOLDbot. Here
+          at BOLDbot, we are committed to protecting your personal information
+          and your right to privacy. If you have any questions or concerns about
+          our policy or our practices with regard to your personal information,
+          please contact us at boldbotofficial@gmail.com. When you visit our
+          Mobile Application BOLDbot (“Mobile Application”) and use our
+          services, you trust us with your personal information. We take your
+          privacy very seriously. In this privacy notice, we describe our
+          privacy policy. We seek to explain to you in the clearest way possible
+          what information we collect, how we use it, and what rights you have
+          concerning it. We hope you take some time to read through it
+          carefully, as it is important. If there are any terms in this privacy
+          policy that you do not agree with, please discontinue the use of our
+          Mobile Application and our services.
+        </p>
+        <h3>ABOUT US</h3>
+        <p>
+          BOLDbot is the next step in automating customer service for Instagram
+          businesses. It levels up the professionalism of your business by
+          increasing efficacy and simplifying interaction with your consumer. We
+          are primarily using the Instagram Messenger API provided by Facebook
+          to handle DM automation of business/professional accounts on
+          Instagram. We provide our services as a subscription model to all
+          business/professional accounts across India.
+        </p>
+        <br />
+        <strong>
+          Please read this privacy policy carefully as it will help you make
+          informed decisions about sharing your personal information with us.
+        </strong>
+        <h3>1. WHAT INFORMATION DO WE COLLECT?</h3>
+        <p>
+          We collect personal information that you voluntarily provide to us
+          when registering with us, expressing an interest in obtaining
+          information about us or our products and services, when participating
+          in activities on the Mobile Application, or otherwise contacting us.
+          The personal information that we collect depends on the context of
+          your interactions with us and the Mobile Application, the choices you
+          make, and the services and features you use. The personal information
+          we collect can include the following:
+        </p>
+        <br />
+        <strong>Name and Contact Data:</strong>
+        <p>
+          We collect your Name, Email Address, Facebook Page ID, and Instagram
+          page details.
+        </p>
+        <br />
+        <strong>
+          We do not collect and store any information regarding your messages or
+          personal DMs on Instagram. We use webhooks to process data and reply
+          automatically.
+        </strong>
+        <br />
+        <br />
+        <strong>Payment Data:</strong>
+        <p>
+          We collect data necessary to process your payments if you choose to
+          subscribe to a plan, such as your payment instrument number (such as a
+          credit card number), and the security code associated with your
+          payment instrument. All payment data is stored by our payment
+          processor and you should review its privacy policies and contact the
+          payment processor directly to respond to your questions.
+        </p>
+        <br />
+        <strong>Information automatically collected</strong>
+        <p>
+          We automatically collect certain information when you visit, use or
+          navigate the Mobile Application. This information does not reveal your
+          specific identity (like your name or contact information) but may
+          include device and usage information, such as your IP address,
+          browser, and device characteristics, operating system, language
+          preferences, referring URLs, device name, country, location,
+          information about how and when you use our Mobile Application and
+          other technical information. If you access our Mobile Application with
+          your mobile device, we may automatically collect device information
+          (such as your mobile device ID, model, and manufacturer), operating
+          system, version information, and IP address. This information is
+          primarily needed to maintain the security and operation of our Mobile
+          Application, and for our internal analytics and reporting purposes.
+          Like many businesses, we also collect information through cookies and
+          similar technologies. You can find out more about this in our Cookie
+          Policy.
+        </p>
+        <br />
+        <strong>Information collected from other sources</strong>
+        <p>
+          We may obtain information about you from other sources, such as public
+          databases, joint marketing partners, social media platforms (such as
+          Instagram), as well as from other third parties.
+        </p>
+        <br />
+        <h3>2. HOW DO WE USE YOUR INFORMATION?</h3>
+        <p>
+          We use your personal information for these purposes in reliance on our
+          legitimate business interests (“Business Purposes”), to enter into or
+          perform a contract with you (“Contractual”), with your consent
+          (“Consent”), and/or for compliance with our legal obligations (“Legal
+          Reasons”). We indicate the specific processing grounds we rely on next
+          to each purpose listed below.
+        </p>
+        <p>We use the information we collect or receive:</p>
+        <p>
+          • To automate your DMs, and provide you with services like Ice
+          Breakers, Persistent Menu, Custom Replies, Greetings, Story Replies
+          and Mentions.
+        </p>
+        <p>
+          • To send administrative information to you related to your account,
+          our business purposes, and/or for legal reasons. We may use your
+          personal information to send you a product, service, new feature
+          information, and/or information about changes to our terms,
+          conditions, and policies.
+        </p>
+        <p>
+          • Deliver targeted advertising to you for our Business Purposes and/or
+          with your Consent. We may use your information to develop and display
+          content and advertising (and work with third parties who do so)
+          tailored to your interests and/or location and to measure its
+          effectiveness. For more information, see our Cookie Policy.
+        </p>
+        <p>
+          • Request Feedback for our Business Purposes and/or with your Consent.
+          We may use your information to request feedback and to contact you
+          about your use of our Mobile Application.
+        </p>
+        <p>
+          • To protect our Mobile Application for Business Purposes and/or Legal
+          Reasons. We may use your information as part of our efforts to keep
+          our Mobile Application safe and secure (for example, for fraud
+          monitoring and prevention).
+        </p>
+        <p>
+          • To enforce our terms, conditions, and policies for our business
+          purposes and as legally required.
+        </p>
+        <p>
+          • To respond to legal requests and prevent harm as legally required.
+          If we receive a subpoena or other legal request, we may need to
+          inspect the data we hold to determine - how to respond.
+        </p>
+        <p>
+          • For other Business Purposes. We may use your information for other
+          Business Purposes, such as data analysis, identifying usage trends,
+          determining the effectiveness of our promotional campaigns, and
+          evaluating and improving our Mobile Application, services, marketing,
+          and experience.
+        </p>
+        <h3>3. WILL YOUR INFORMATION BE SHARED WITH ANYONE?</h3>
+        <p>
+          We only share and disclose your information in the following
+          situations:
+        </p>
+        <p>
+          • Compliance with Laws. We may disclose your information where we are
+          legally required to do so to comply with applicable law, governmental
+          requests, a judicial proceeding, court order, or legal processes, such
+          as in response to a court order or a subpoena (including in response
+          to public authorities to meet national security or law enforcement
+          requirements).
+        </p>
+        <p>
+          • Vital Interests and Legal Rights. We may disclose your information
+          where we believe it is necessary to investigate, prevent, or take
+          action regarding potential violations of our policies, suspected
+          fraud, situations involving potential threats to the safety of any
+          person, and illegal activities, or as evidence in litigation in which
+          we are involved.
+        </p>
+        <p>
+          • With your Consent. We may disclose your personal information for any
+          other purpose with your consent.
+        </p>
+        <p>
+          • Other Users. When you share personal information (for example, by
+          posting comments, contributions, or other content to the Mobile
+          Application) or otherwise interact with public areas of the Mobile
+          Application, such personal information may be viewed by all users and
+          may be publicly distributed outside the Mobile Application in
+          perpetuity.
+        </p>
+        <h3>4. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</h3>
+        <p>
+          We may use cookies and similar tracking technologies (like web beacons
+          and pixels) to access or store information. Specific information about
+          how we use such technologies and how you can refuse certain cookies is
+          set out in our Cookie Policy.
+        </p>
+        <h3>5. IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?</h3>
+        <p>
+          Information collected from you may be stored and processed globally in
+          various countries in which our Company or agents or contractors
+          maintain facilities, and by accessing our sites and using our
+          services, you consent to any such transfer of information outside of
+          your country. Such countries may have laws that are different, and
+          potentially not as protective, as the laws of your own country.
+          Whenever we share personal data originating in the European Economic
+          Area we will rely on lawful measures to transfer that data, such as
+          the Privacy Shield or the EU standard contractual clauses. If you
+          reside in the EEA or other regions with laws governing data collection
+          and use, please note that you are agreeing to the transfer of your
+          personal data to the United States and other countries in which we
+          operate. By providing your personal data, you consent to any transfer
+          and processing in accordance with this Policy. We will not transfer
+          your personal information to an overseas recipient
+        </p>
+        <h3>6. WHAT IS OUR STANCE ON THIRD-PARTY WEBSITES?</h3>
+        <p>
+          The Mobile Application may contain advertisements from third parties
+          that are not affiliated with us and which may link to other websites,
+          online services, or mobile applications. We cannot guarantee the
+          safety and privacy of the data you provide to any third parties. Any
+          data collected by third parties is not covered by this privacy policy.
+          We are not responsible for the content or privacy and security
+          practices and policies of any third parties, including other websites,
+          services, or applications that may be linked to or from the Mobile
+          Application. You should review the policies of such third parties and
+          contact them directly to respond to your questions.
+        </p>
+        <h3>7. HOW LONG DO WE KEEP YOUR INFORMATION?</h3>
+        <p>
+          We will only keep your personal information for as long as it is
+          necessary for the purposes set out in this privacy policy unless a
+          longer retention period is required or permitted by law (such as tax,
+          accounting, or other legal requirements). When we have no ongoing
+          legitimate business need to process your personal information, we will
+          either delete or anonymize it, or, if this is not possible (for
+          example, because your personal information has been stored in backup
+          archives), then we will securely store your personal information and
+          isolate it from any further processing until deletion is possible.
+        </p>
+        <h3>8. HOW DO WE KEEP YOUR INFORMATION SAFE?</h3>
+        <p>
+          We have implemented appropriate technical and organizational security
+          measures designed to protect the security of any personal information
+          we process. However, please also remember that we cannot guarantee
+          that the internet itself is 100% secure. Although we will do our best
+          to protect your personal information, the transmission of personal
+          information to and from our Mobile Application is at your own risk.
+          You should only access the services within a secure environment.
+        </p>
+        <h3>9. WHAT ARE YOUR PRIVACY RIGHTS?</h3>
+        <p>
+          Account Information: You may at any time review or change the
+          information in your account or terminate your account by:
+        </p>
+
+        <p>• Contacting us using the contact information provided below:</p>
+        <p>
+          Upon your request to terminate your account, we will deactivate or
+          delete your account and information from our active databases.
+          However, some information may be retained in our files to prevent
+          fraud, troubleshoot problems, assist with any investigations, enforce
+          our Terms of Use, and/or comply with legal requirements.
+        </p>
+        <p>
+          Cookies and similar technologies: Most Web browsers are set to accept
+          cookies by default. If you prefer, you can usually choose to set your
+          browser to remove cookies and reject cookies. If you choose to remove
+          cookies or reject cookies, this could affect certain features or
+          services of our Mobile Application.
+        </p>
+        <h3>10. DO WE MAKE UPDATES TO THIS POLICY?</h3>
+        <p>
+          We may update this privacy policy from time to time. The updated
+          version will be indicated by an updated “Revised” date and the updated
+          version will be effective as soon as it is accessible. If we make
+          material changes to this privacy policy, we may notify you either by
+          prominently posting a notice of such changes or by directly sending
+          you a notification. We encourage you to review this privacy policy
+          frequently to be informed of how we are protecting your information.
+        </p>
+        <h3>11. HOW CAN YOU CONTACT US ABOUT THIS POLICY?</h3>
+        <p>
+          If you have questions or comments about this policy, email us at
+          boldbotofficial@gmail.com.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default PrivacyPolicy;
