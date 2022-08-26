2a8308f234431402cb9538ff91934a4cc091913b
avivashishta29 <avivashishta@gmail.com>
2022-08-26 06:55:08 +0530

    next pwa

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ import "../styles/App.scss";
 import Script from "next/script";
 import SEO from "../components/SEO";
 import RouteGuard from "../components/RouteGuard";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const dispatch = useDispatch();
@@ -26,6 +27,25 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="BOLDbot is the next step in automating customer service for Instagram
+          businesses. It levels up the professionalism of your business by
+          increasing efficacy and simplifying interaction with your consumer."
+        />
+        <meta name="theme-color" content="#3781e3" />
+        <title>BOLDbot</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+      </Head>
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
