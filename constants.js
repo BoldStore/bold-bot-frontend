689cfbd46380a045eed4a82ee459a6af21c8051e
avivashishta29 <avivashishta@gmail.com>
2022-09-07 11:04:42 +0530

    constants change

--- constants.js
+++ constants.js
@@ -4,7 +4,8 @@ export const FACEBOOK_AUTH_URL = "https://www.facebook.com/v14.0/dialog/oauth";
 
 export const CLIENT_ID = "1187703702070626";
 
-export const SCOPE = "instagram_manage_messages";
+export const SCOPE =
+  "instagram_manage_messages,pages_show_list,pages_read_engagement";
 
 export const REDIRECT_URI = "https://boldbot.in/facebook-page/";
 
