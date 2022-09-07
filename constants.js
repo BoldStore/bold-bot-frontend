8f5ed2f5d6a991c485be9efada459cf74f8c69e0
avivashishta29 <avivashishta@gmail.com>
2022-09-07 11:45:48 +0530

    more permissions

--- constants.js
+++ constants.js
@@ -5,7 +5,7 @@ export const FACEBOOK_AUTH_URL = "https://www.facebook.com/v14.0/dialog/oauth";
 export const CLIENT_ID = "1187703702070626";
 
 export const SCOPE =
-  "instagram_manage_messages,pages_show_list,pages_read_engagement";
+  "instagram_manage_messages,pages_show_list,pages_read_engagement,pages_manage_metadata";
 
 export const REDIRECT_URI = "https://boldbot.in/facebook-page/";
 
