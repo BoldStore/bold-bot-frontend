40ecde652a0ccb085ae242297b36521e954f63f4
aryanteng <aryanteng892@gmail.com>
2023-01-01 18:57:17 +0530

    form packages

--- components/ContactUs/index.js
+++ components/ContactUs/index.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import styles from "./styles.module.css";
-import { motion } from "framer-motion";
-import InputComponent from "../DashboardComponents/InputComponent";
-import DashboardButton from "../DashboardComponents/DashboardButton";
-import { contactUs } from "./contactUs";
-import { toast } from "react-toastify";
-import { useDispatch } from "react-redux";
-import { contactBold } from "../../store/actions/contact";
-import { useRouter } from "next/router";
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+import { motion } from 'framer-motion';
+import InputComponent from '../DashboardComponents/InputComponent';
+import DashboardButton from '../DashboardComponents/DashboardButton';
+import { contactUs } from './contactUs';
+import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import { contactBold } from '../../store/actions/contact';
+import { useRouter } from 'next/router';
 
 function ContactUs() {
   const dispatch = useDispatch();
@@ -15,33 +15,33 @@ function ContactUs() {
 
   const [form, setForm] = useState([
     {
-      key: "email",
-      value: "",
+      key: 'email',
+      value: '',
     },
     {
-      key: "username",
-      value: "",
+      key: 'username',
+      value: '',
     },
   ]);
 
   const validate = () => {
     if (
-      form.find((item) => item.key == "email").value === "" &&
-      form.find((item) => item.key == "username").value === ""
+      form.find((item) => item.key == 'email').value === '' &&
+      form.find((item) => item.key == 'username').value === ''
     ) {
-      toast.error("Please fill in your details!");
+      toast.error('Please fill in your details!');
       return false;
     }
-    if (form.find((item) => item.key == "email").value === "") {
-      toast.error("Please fill in your email!");
+    if (form.find((item) => item.key == 'email').value === '') {
+      toast.error('Please fill in your email!');
       return false;
     }
-    if (form.find((item) => item.key == "username").value === "") {
-      toast.error("Please fill in your username!");
+    if (form.find((item) => item.key == 'username').value === '') {
+      toast.error('Please fill in your username!');
       return false;
     }
-    if (!form.find((item) => item.key == "email").value.includes("@")) {
-      toast.error("Please fill a valid email address!");
+    if (!form.find((item) => item.key == 'email').value.includes('@')) {
+      toast.error('Please fill a valid email address!');
       return false;
     }
     return true;
@@ -51,14 +51,14 @@ function ContactUs() {
     if (validate()) {
       dispatch(
         contactBold(
-          form.find((e) => (e.key = "email")).value,
-          form.find((e) => (e.key = "username")).value
+          form.find((e) => (e.key = 'email')).value,
+          form.find((e) => (e.key = 'username')).value
         )
       );
-      toast.success("Form Submitted Successfully!", {
+      toast.success('Form Submitted Successfully!', {
         autoClose: 3000,
       });
-      router.push("/");
+      router.push('/');
     }
     return;
   };
@@ -80,28 +80,28 @@ function ContactUs() {
     setForm(inputs);
   };
   return (
-    <div className={styles.container} id="ContactUs">
+    <div className={styles.container} id='ContactUs'>
       <div className={styles.backgroundGradient} />
       <div className={styles.backgroundGradientMobile} />
       <div className={styles.contactUs}>
         <h2>Contact Us</h2>
         <h6>
-          Want your own BOLD<span style={{ color: "var(--blue)" }}>bot</span>?{" "}
-          Fill in your details below or DM us{" "}
+          Want your own BOLD<span style={{ color: 'var(--blue)' }}>bot</span>?{' '}
+          Fill in your details below or DM us{' '}
           <a
-            href="https://www.instagram.com/boldbot.in/"
-            target={"_blank"}
+            href='https://www.instagram.com/boldbot.in/'
+            target={'_blank'}
             className={styles.link}
-            rel="noreferrer"
+            rel='noreferrer'
           >
             @boldbot.in
-          </a>{" "}
+          </a>{' '}
           !
         </h6>
-        {contactUs.map((item, i) => {
+        {contactUs.map((item) => {
           return (
             <InputComponent
-              key={i}
+              key={item.title}
               title={item.title}
               placeholder={item.placeholder}
               desc={item.desc}
@@ -111,17 +111,17 @@ function ContactUs() {
             />
           );
         })}
-        <DashboardButton text={"Submit"} onClick={submit} />
+        <DashboardButton text={'Submit'} onClick={submit} />
       </div>
       <motion.img
-        src={"/assets/boldbotFeed.png"}
+        src={'/assets/boldbotFeed.png'}
         className={styles.img}
         animate={{
           y: [0, 10, 0],
         }}
         transition={{
           duration: 2,
-          type: "spring",
+          type: 'spring',
           repeat: Infinity,
         }}
       />
